b0d87708d14ccb1471be22ee9ebd74671f72acf2
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2021-05-13 09:22:47 +0200

    Ajout des fichier nécessaire a l'étape 2
    File add :
    - Objet.java
    - TestObjet.java
    - Plateau.java
    - TestPlateau.java

---
+++ CODE/src/tests/TestObjet.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class TestObjet {
+    //main
+    public static void main(String[] args) {
+        // A completer
+    }
+}
---
+++ CODE/src/tests/TestPlateau.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class TestPlateau {
+    //main
+    public static void main(String[] args) {
+        // A completer
+    }
+}
