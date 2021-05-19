57f2050804e4cddbe9cb5facb6f4111b33a0953e
Cyril Berche <berchecyril@gmail.com>
2021-05-19 18:33:44 +0200

    Test nettoyage

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"tests","l":"MaDemoIG"},{"p":"composants","l":"Piece"},{"p":"composants","l":"PieceM0"},{"p":"composants","l":"PieceM1"},{"p":"composants","l":"PieceM2"},{"p":"tests","l":"TestPieces"},{"p":"composants","l":"Utils"}];updateSearchResults();
