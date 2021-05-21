ca7d530807163e8e8e68d10af5a2e02a453fa71c
dzpower59 <naim.parcoursup@gmail.com>
2021-05-21 11:54:23 +0200

    Test Joueur a fonnddd

--- CODE/src/tests/TestJoueur.java
+++ CODE/src/tests/TestJoueur.java
@@ -9,6 +9,7 @@ public class TestJoueur {
     //main
     public static void main(String[] args) {
         //création de la fenêtre de jeu
+
         Object[] parametresJeu = IG.saisirParametres();
         int nbJoueurs = (Integer)parametresJeu[0];
         IG.creerFenetreJeu("Démo Librairie IG version 1.9", nbJoueurs);
@@ -22,7 +23,24 @@ public class TestJoueur {
         message[2] = "Cliquez pour continuer ... ";
         message[3] = "";
         IG.afficherMessage(message);
+
+        //Placement des piece aleatoire
+
+        for (int i=0; i<7; i++) {
+            for (int j=0; j<7; j++) {
+                Piece piece = plateau.getPiece(i,j);
+                IG.changerPiecePlateau(i,j,piece.getModelePiece(),piece.getOrientationPiece());
+            }
+        }
+        IG.changerPieceHorsPlateau(pieceHorsPlateau.getModelePiece(),pieceHorsPlateau.getOrientationPiece());
+        for(Joueur joueur : joueurs){
+            IG.placerJoueurSurPlateau(numJoueur.getNumJoueur(),nomJoueur.getNomJoueur());
+        }
         IG.rendreVisibleFenetreJeu();
         IG.attendreClic();
+
+
+        //Nombre d'objet récupérés par le joueur
+
     }
 }
